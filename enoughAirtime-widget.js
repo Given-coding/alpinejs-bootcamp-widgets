fd83d74f11f3836577b157cca516379c195bfa80
Given_Ledwaba <99291523+Given-coding@users.noreply.github.com>
2022-07-07 11:49:14 +0200

    modefide

---
+++ enoughAirtime-widget.js
@@ -0,0 +1,17 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('enoughAirtimeWidget', function () {
+        return {
+                airtimeUsage : '',
+                availableAmount : '0.00',
+                airtimeMessage : '',
+                calculate(){
+                    this.airtimeMessage = enoughAirtime(this.airtimeUsage, this.availableAmount)
+                    setTimeout(() => {
+                        this.airtimeMessage = '';
+                        this.airtimeUsage = '';
+                        this.availableAmount = '0.00';
+                    }, 3000);
+                }
+        }
+    });
+})
---
+++ wordCrush-widget.js
@@ -0,0 +1,24 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('wordCrushWidget', function () {
+        return {
+            sentence : '',
+            word: '',
+            LongestWord: '',
+            ShortestWord: '',
+            showWord: false,
+            WordLength: '',
+            play(){
+                this.LongestWord = longestWord(this.word, this.sentence);
+                this.ShortestWord = shortestWord(this.word, this.sentence);
+                this.WordLength = wordLengths(this.word, this.sentence);
+                setTimeout(() => {
+                    this.showWord=false;
+                    this. word = '';
+                    this.LongestWord = '';
+                    this.ShortestWord = '';
+                    this.WordLength = '';
+                }, 3000);
+            }
+        }
+    });
+})
